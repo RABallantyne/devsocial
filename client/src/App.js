d8aeb6328515349082d20ad72641f5472bd362ed
Jerkbert <raballantyne@gmail.com>
2020-03-12 14:49:22 -0600

    add/edit profile action created

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-forms/CreateProfile";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 //Redux stuff
-import { Provider } from "react-redux";
-import store from "./store";
-import Alert from "./components/layout/Alert";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import "./App.css";
-import { loadUser } from "./actions/auth";
-import setAuthToken from "./utils/setAuthToken";
+import { Provider } from 'react-redux';
+import store from './store';
+import Alert from './components/layout/Alert';
+import PrivateRoute from './components/routing/PrivateRoute';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -29,16 +30,16 @@ const App = () => {
       <Router>
         <>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
             <Alert />
             <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
-                path="/create-profile"
+                path='/create-profile'
                 component={CreateProfile}
               />
             </Switch>
