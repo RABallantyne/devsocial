d1de53daf42d357a185316c13e8cc4edddbd89be
Jerkbert <raballantyne@gmail.com>
2020-03-13 09:28:52 -0600

    edit profile fully functioning

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
 //Redux stuff
 import { Provider } from 'react-redux';
 import store from './store';
-import Alert from './components/layout/Alert';
-import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
@@ -41,6 +42,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
